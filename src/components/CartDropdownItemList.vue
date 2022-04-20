5664cd950ced244548a9a4004011afe5159bbe1a
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-04-20 12:17:00 +0700

    Create CartDropdownItemList.vue

---
+++ src/components/CartDropdownItemList.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- implement better dropdown via other css framework -->
+  <select name="cars" id="cars" class="dropdown">
+    <option>title x amount</option>
+    <option v-for="item in cart">{{item.title}} x {{item.amount}}</option>
+    <div></div>
+  </select>
+</template>
+
+<script>
+import { computed } from "vue";
+import { useStore } from "vuex";
+export default {
+  setup() {
+    const { getters } = useStore();
+
+    const cart = computed(() => getters["cart/cart"]);
+
+    return { cart };
+  },
+};
+</script>
+
+<style>
+</style>
