68c0f8edae00d44c7293ca089fc71967115ac1b2
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-04-20 12:14:10 +0700

    implement productlist

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -1,4 +1,33 @@
 <template>
+  <div class="product-wrapper">
+    <ProductCard v-for="product in products" :product="product" />
+  </div>
 </template>
+
 <script>
+import ProductCard from "./ProductCard.vue";
+
+import { createNamespacedHelpers } from "vuex";
+const { mapState, mapActions } = createNamespacedHelpers("product");
+
+export default {
+  components: { ProductCard },
+  computed: {
+    ...mapState({ products: (state) => state.products }),
+  },
+  methods: {
+    ...mapActions(["setProducts"]),
+  },
+  async created() {
+    await this.setProducts();
+  },
+};
 </script>
+
+<style scoped>
+.product-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  /* grid-template-columns: repeat(4, 1fr); */
+}
+</style>
