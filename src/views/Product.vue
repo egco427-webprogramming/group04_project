dd3e78caf3563f8efbf448d3a102c33d89f171f2
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-04-20 12:14:43 +0700

    implement product page

---
+++ src/views/Product.vue
@@ -0,0 +1,29 @@
+<template>
+  <suspense>
+    <template #default>
+      <ProductDetail :id="id" />
+    </template>
+    <template #fallback>
+      loading..
+      <!-- implement skeleton loading later -->
+    </template>
+  </suspense>
+</template>
+
+<script>
+import { useRoute } from "vue-router";
+import ProductDetail from "../components/ProductDetail.vue";
+// import { createNamespacedHelpers, useStore } from "vuex";
+// const { mapState, mapActions } = createNamespacedHelpers("product");
+
+export default {
+  setup() {
+    const id = useRoute().params.id;
+    return { id };
+  },
+  components: { ProductDetail },
+};
+</script>
+
+<style>
+</style>
