647c7a56c622b96d955729a117333012d45ea8bf
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-04-21 16:26:34 +0700

    user setting controller page

---
+++ src/components/UserIndex.vue
@@ -0,0 +1,40 @@
+<template>
+  <UserSetting :user="user" />
+  <UserHistory :history="[{id:'a1',total:'100',date:'1/2/2020'}]" />
+</template>
+
+<script>
+import UserSetting from "./UserSetting.vue";
+import UserHistory from "./UserHistory.vue";
+
+import { getUser } from "../services/user.service";
+import { ref } from "vue";
+
+export default {
+  components: {
+    UserSetting,
+    UserHistory,
+  },
+  props: {
+    id: String,
+  },
+  async setup(props) {
+    const user = ref(await getUser(props.id));
+    return { user };
+  },
+  methods: {
+    async getUser(id) {
+      let user;
+      try {
+        user = await getUser(id);
+      } catch (err) {
+        console.log(err);
+      }
+      return user;
+    },
+  },
+};
+</script>
+
+<style>
+</style>
