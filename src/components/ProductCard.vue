d183db8922e5594ee22ae61cb2f31e703f66774b
Nicharee  Chalermsuksri <nicharee.chl@student.mahidol.edu>
2022-04-22 20:49:22 +0700

    edit Product Card

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,36 +1,24 @@
 <template>
-  <!-- <div class="card">
-    <img :src="product.image" :alt="product.title" width="100" />
-    <router-link :to="{name:'Product',params:{id:product.id}}">
-      <h4>{{product.title}}</h4>
-      <h4>{{product.price}}</h4> -->
+    <sui-card>
+      <!-- product image -->
+      <sui-image :src="product.image" :alt="product.title" class="card-image"/>
+      <sui-card-content  class="card-content" align="left">
+        <!-- produvt title -->
+        <sui-card-header>
+            {{product.title}}
+        </sui-card-header>
+        <!-- product price -->
+        <sui-card-meta>
+          <span class="price">${{product.price}}</span>
+        </sui-card-meta>
+      </sui-card-content>
+      <!-- add button -->
+      <sui-button @click.prevent="addToCart" attached="bottom" >
+        <sui-icon name="cart plus" />
+        Add to cart
+      </sui-button>
+    </sui-card>
 
-      <!-- <p>{{product.description}}</p> -->
-    <!-- </router-link>
-    <h6>{{product.category}}</h6>
-    <button @click.prevent="addToCart">add to cart</button>
-  </div> -->
-
-  <sui-card>
-    <!-- product image -->
-    <sui-image :src="product.image" :alt="product.title" width="100" hight="300" align="center"/>
-    <sui-card-content align="left">
-      <!-- produvt title -->
-      <sui-card-header>
-          {{product.title}}
-      </sui-card-header>
-      <!-- product price -->
-      <sui-card-meta>
-        <span class="price">฿3,800</span>
-      </sui-card-meta>
-    </sui-card-content>
-    <!-- add button -->
-    <sui-button @click.prevent="addToCart" attached="bottom" >
-      <sui-icon name="add" />
-      Add to cart
-    </sui-button>
-
-  </sui-card>
 </template>
 
 <script>
@@ -58,19 +46,22 @@ a {
   text-decoration: none;
 }
 .card {
-  max-width: 300px;
-  width: 100%;
-}
-.img {
-  max-width: 300px;
-  width: 100%;
-}
-img:hover {
+  width: 300px;
+  height: 450px;
   cursor: pointer;
-  opacity: 0.9;
-  transition: all 0.1s ease-in-out;
+  margin-top: 0px;
+  margin-left: 10px;
+  margin-right: 10px;
+  margin-bottom: 30px;
 }
-.buttons {
-  gap: 5px;
+.card-content {
+  align-items: center;
 }
-</style>
+.card-image {
+  width: 300px;
+  height: 300px;
+  object-fit: cover;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style> 
