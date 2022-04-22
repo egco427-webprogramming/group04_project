cc7369ab6f6f1d3285b57d0e1954f0dd4bb94c0f
Nicharee  Chalermsuksri <nicharee.chl@student.mahidol.edu>
2022-04-22 20:47:30 +0700

    remove unused code in homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,15 @@
 <template>
-    <MyNavbar />
     <TheCarousel />
-    <sui-segment>
-    <sui-segment id="upper-segment" color="teal">
-        <div>
-            <sui-label ribbon color="teal">Contacts</sui-label>
-        </div>
-        <div>
-            <form @submit.prevent="submitHandler">
-            <sui-input fluid placeholder="Search..." v-model="query" />
-            </form>
-        </div>
-    </sui-segment>
-    </sui-segment>
-    <TheFooter />
 </template>
  
 <script>
     import TheCarousel from "../components/TheCarousel.vue";
-    import MyNavbar from "../components/MyNavbar.vue";
-    import TheFooter from "../components/TheFooter.vue";
+
     export default {
         name: 'Home',
 
         components: {
             TheCarousel,
-            TheFooter,
-            MyNavbar,
         },
     }
 </script>
