4fe397b341dad8fe95e48cccb60ecdb4804a4ba8
Nicharee  Chalermsuksri <nicharee.chl@student.mahidol.edu>
2022-04-22 14:45:26 +0700

    add Footer

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,38 @@
+<template>
+    <footer id="contact" bg="black">
+        <br><br>
+        <div class="container">               
+            <!-- Contact -->
+            <div class="col-12 text-center">
+                <h2 class="display-1"><b>Contact</b></h2>
+            </div>
+            <div class="row py-4">
+                <div class="col-12 text-center">
+                    <sui-icon name="facebook square" />
+                    <sui-icon name="twitter square" />
+                    <sui-icon name="instagram" />
+                    <sui-icon name="envelope outline" />
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <p class="text-center m-0 text-secondary">Lorem ipsum dolor sit amet Lorem ipsum dolor sit</p>
+                    <p class="text-center m-0 text-secondary">CopyRight &copy; Nicharee</p>
+                </div>
+            </div>
+        </div>
+        <br><br>
+    </footer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.sui-icon {
+    /*  */
+}
+</style>
