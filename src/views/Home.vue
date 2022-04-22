3c57ab7f5ec15c5e1046b50ac0afa6e6c8cecc76
Nicharee  Chalermsuksri <nicharee.chl@student.mahidol.edu>
2022-04-22 14:50:37 +0700

    add Navbar, Carousel, Footer

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,46 @@
 <template>
-<h1>Home</h1>
+    <MyNavbar />
+    <TheCarousel />
+    <sui-segment>
+    <sui-segment id="upper-segment" color="teal">
+        <div>
+            <sui-label ribbon color="teal">Contacts</sui-label>
+        </div>
+        <div>
+            <form @submit.prevent="submitHandler">
+            <sui-input fluid placeholder="Search..." v-model="query" />
+            </form>
+        </div>
+    </sui-segment>
+    </sui-segment>
+    <TheFooter />
 </template>
+ 
 <script>
-export default {};
+    import TheCarousel from "../components/TheCarousel.vue";
+    import MyNavbar from "../components/MyNavbar.vue";
+    import TheFooter from "../components/TheFooter.vue";
+    export default {
+        name: 'Home',
+
+        components: {
+            TheCarousel,
+            TheFooter,
+            MyNavbar,
+        },
+    }
 </script>
+    
 
 <style>
+#upper-segment {
+  display: grid;
+  grid-template-columns: 1fr 13fr 2fr;
+  gap: 1%;
+  align-items: center;
+  justify-items: center;
+}
+#upper-segment :nth-child(2) {
+  width: 100%;
+}
 </style>
