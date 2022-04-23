b868f27892c428f9d2aa1439b1060ab15952c9e0
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-04-23 13:51:30 +0700

    binding data in cart result

---
+++ src/components/CartIndex.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>cart:{{cart}}</div>
+  <div>total:{{totalPrice}}</div>
+  <div>discount:{{totalDiscount}}</div>
+  <div>Result:{{totalResult}}</div>
+</template>
+
+<script>
+import { computed } from "vue";
+import { useStore } from "vuex";
+export default {
+  setup() {
+    const { getters } = useStore();
+
+    const cart = computed(() => getters["cart/cart"]);
+    const totalPrice = computed(() => getters["cart/totalPrice"]);
+    const totalDiscount = computed(() => getters["cart/totalDiscount"]);
+    const totalResult = computed(() => getters["cart/totalResult"]);
+    return { cart, totalPrice, totalDiscount, totalResult };
+  },
+};
+</script>
+
+<style>
+</style>
