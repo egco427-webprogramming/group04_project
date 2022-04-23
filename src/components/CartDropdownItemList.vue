63ba059e164446557e7ee7f2e5d50932ba496f7b
Nicharee  Chalermsuksri <nicharee.chl@student.mahidol.edu>
2022-04-24 03:19:08 +0700

    upgrade CartDropdownItemList

--- src/components/CartDropdownItemList.vue
+++ src/components/CartDropdownItemList.vue
@@ -1,30 +1,39 @@
 <template>
-  <!-- implement better dropdown via other css framework -->
-  <!-- <select name="cars" id="cars" class="dropdown">
-    <option>name x amount</option>
-    <option v-for="item in cart">{{item.name}} x {{item.amount}}</option>
-    <div></div>
-  </select>-->
-  <!-- <div class="ui dropdown">
-    <div class="text">Cart</div>
+  <div class="ui simple dropdown item">
+    <sui-icon name="shopping cart" />
+    THB {{totalPrice}}
     <i class="dropdown icon"></i>
     <div class="menu">
-      <div class="item" v-for="item in cart">{{item.name}} x {{item.amount}}</div>
-    </div>
-  </div>-->
-  <sui-dropdown :text="'THB '+totalPrice">
-    <sui-dropdown-menu>
-      <sui-dropdown-item
-        v-for="item in cart"
-        :text="item.name+' x '+item.amount"
-        :description=" 'THB ' + String(Math.round(item.price*item.amount))"
-      />
-
-      <div class="item">
-        <router-link :to="{name:'Cart'}">buy</router-link>
+      <div class="item" v-for="item in cart"> 
+          <div class="d-flex flex-row justify-content-between my-flex-container">
+            <div class="col">
+              <span class="product-name">{{item.name}} x {{item.amount}}</span>
+              <br>
+              <span>THB {{String(Math.round(item.price*item.amount))}}</span>
+            </div>
+            
+            <div class="col" align="right">
+              &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+              <button class="ui button"><sui-icon name="trash alternate"/>delete</button>
+            </div>
+          </div> 
       </div>
-    </sui-dropdown-menu>
-  </sui-dropdown>
+      <div class="item" >
+        <div v-if="cart.length !== 0">
+          <router-link :to="{name:'Cart'}">
+              <div class="d-grid gap-2 col-6 mx-auto" align="center" >
+                  <button class="large ui secondary button">Buy</button>
+              </div>   
+          </router-link>
+        </div>
+        <template v-else>
+            <p class="empty-bag-icon"><br><sui-icon name="shopping cart" /></p>
+            <br>
+            <p class="empty-bag-text">YOUR CART IS EMPTY</p>
+        </template>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -38,8 +47,35 @@ export default {
     const totalPrice = computed(() => getters["cart/totalPrice"]);
     return { cart, totalPrice };
   },
+  
 };
 </script>
 
 <style>
+a {
+  text-decoration: none;
+}
+br {
+  display: block; /* makes it have a width */
+  content: ""; /* clears default height */
+  margin-top: 10px; /* change this to whatever height */
+}
+.product-name{
+  font-weight: bold;
+}
+.empty-bag-icon{
+  font-size:35px;
+  line-height:0px;
+  margin:0 8px;
+  color:rgb(186, 183, 183);
+  opacity:0.75;
+  text-align:center;
+}
+.empty-bag-text{
+  font-size:15px;
+  font-weight: bold;
+  margin: auto;
+  color:rgb(162, 160, 160);
+  opacity:0.75;
+}
 </style>
