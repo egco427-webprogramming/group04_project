2e9a3b2a42fda6585292e8bef92d702a577fa7a2
Jao <Jaopatk@gmail.com>
2022-04-23 12:07:00 +0700

    update basic user

--- src/components/UserSetting.vue
+++ src/components/UserSetting.vue
@@ -1,58 +1,61 @@
 <template>
   <form class="ui form">
     <p>{{user}}</p>
-    <h4 class="ui dividing header">Shipping Information</h4>
-    <div class="field">
-      <label>Name</label>
-      <div class="two fields">
-        <div class="field">
-          <input type="text" v-model="user.name.firstname" placeholder="First Name" />
-        </div>
-        <div class="field">
-          <input type="text" v-model="user.name.lastname" placeholder="Last Name" />
-        </div>
+    <h4 class="ui dividing header">Edit Profile</h4>
+      <div class="field">
+        <label>Username</label>
+        <input type="text" v-model="user.username" placeholder="First Name" />
+      </div>
+<br>
+    <div class="two fields">
+      <div class="field">
+        <label>First name</label>
+        <input type="text" v-model="user.name.firstname" placeholder="First Name" />
+      </div>
+      <div class="field">
+        <label>Last name</label>
+        <input type="text" v-model="user.name.lastname" placeholder="Last Name" />
       </div>
     </div>
+
     <div class="field">
-      <label>Name</label>
-      <div class="two fields">
-        <div class="field">
-          <input type="text" placeholder="Email" v-model="user.email" />
-        </div>
-        <div class="field">
-          <input type="text" placeholder="Tel" v-model="user.phone" />
-        </div>
+      <div class="field">
+        <label>Email</label>
+        <input type="text" placeholder="Email" v-model="user.email" />
+      </div>
+      <div class="field">
+        <label>Mobile Number</label>
+        <input type="text" placeholder="Tel" v-model="user.phone" />
       </div>
     </div>
+<br>
     <div class="field">
       <label>Shipping Address</label>
-      <div class="fields">
         <div class="field">
-          <input type="text" placeholder="number" v-model="user.address.number" />
+          <textarea rows="2" type="text" placeholder="Address" v-model="user.address.city" />
         </div>
-        <div class="field">
-          <input type="text" placeholder="city" v-model="user.address.city" />
-        </div>
-      </div>
-      <div class="fields">
-        <div class="field">
-          <input type="text" placeholder="Street" v-model="user.address.street" />
-        </div>
-        <div class="field">
-          <input type="text" placeholder="Zipcode" v-model="user.address.zipcode" />
-        </div>
-      </div>
     </div>
+      <button class="ui button blue" type="submit">Update User</button>
+
+      <h4 class="ui dividing header">Purchase History</h4>
+
   </form>
 </template>
 
 <script>
-export default {
-  props: {
-    user: Object,
-  },
-};
+  export default {
+    props: {
+      user: Object,
+    },
+  };
+  
 </script>
 
 <style>
+  .form {
+    margin-left: 20%;
+    margin-right: 20%;
+    text-align: left;
+  }
+
 </style>
