233b7786440f03fbd2a6132844126bb6a064c593
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-04-24 11:45:28 +0700

    handle buy button

--- src/components/CartIndex.vue
+++ src/components/CartIndex.vue
@@ -3,23 +3,52 @@
   <div>total:{{totalPrice}}</div>
   <div>discount:{{totalDiscount}}</div>
   <div>Result:{{totalResult}}</div>
+  <div>id:{{id}}</div>
+
+  <div>user:{{user}}</div>
+
+  <button @click.prevent="buyHandle">buy</button>
 </template>
 
 <script>
-import { computed } from "vue";
+import { computed, ref } from "vue";
 import { useStore } from "vuex";
+import { useRouter } from "vue-router";
+import { getUser, updateUser } from "../services/user.service";
+import { addHistory } from "../services/history.service";
 export default {
   props: {
     id: String,
   },
   async setup(props) {
     const { getters } = useStore();
+    const router = useRouter();
 
     const cart = computed(() => getters["cart/cart"]);
     const totalPrice = computed(() => getters["cart/totalPrice"]);
     const totalDiscount = computed(() => getters["cart/totalDiscount"]);
     const totalResult = computed(() => getters["cart/totalResult"]);
-    return { cart, totalPrice, totalDiscount, totalResult };
+
+    const user = ref(await getUser(props.id));
+
+    const buyHandle = async () => {
+      try {
+        if (cart.value.length == 0) {
+          return alert("Did you forgot something");
+        }
+        await updateUser(props.id, user.value);
+        await addHistory({
+          item: cart.value,
+          uid: props.id,
+          price: totalResult.value,
+        });
+        router.push("/purchased");
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    return { cart, totalPrice, totalDiscount, totalResult, user, buyHandle };
   },
 };
 </script>
