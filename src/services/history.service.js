b79a80cc8e012209255e71876d2c9d8f18b8b080
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-04-24 23:04:08 +0700

    connect backend

--- src/services/history.service.js
+++ src/services/history.service.js
@@ -5,15 +5,24 @@ import axios from "axios";
 // my local api
 const URL = "http://localhost:5000/history";
 
+import { toBackendCart, toFrontendCart } from "../util";
+
 const httpClient = axios.create({
   baseURL: URL,
 });
 
 export async function getHistory(id) {
-  return (await httpClient.get("/id/" + id)).data;
+  return (await httpClient.get("/id/" + id)).data.map(
+    ({ items, ...other }) => ({ items: toFrontendCart(items), ...other })
+  );
 }
 export async function addHistory({ items = [], uid, price }) {
   return (
-    await httpClient.post("/purchased", { items, uid, price, date: Date.now() })
+    await httpClient.post("/purchased", {
+      items: toBackendCart(items),
+      uid,
+      price,
+      date: Date.now(),
+    })
   ).data;
 }
