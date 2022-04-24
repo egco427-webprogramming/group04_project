c44d8217b428f24b98758d1db679e3811125dae0
Nicharee  Chalermsuksri <nicharee.chl@student.mahidol.edu>
2022-04-25 06:41:02 +0700

    delete style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,15 +15,5 @@
 </script>
     
 
-<style>
-#upper-segment {
-  display: grid;
-  grid-template-columns: 1fr 13fr 2fr;
-  gap: 1%;
-  align-items: center;
-  justify-items: center;
-}
-#upper-segment :nth-child(2) {
-  width: 100%;
-}
+<style scoped>
 </style>
