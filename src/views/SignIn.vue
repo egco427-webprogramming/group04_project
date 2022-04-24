b0c0f7ef9ea77ccb233fcc05b01e1e980873be65
Jao <Jaopatk@gmail.com>
2022-04-24 16:58:47 +0700

    update facebook signin

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,25 +1,22 @@
 <template>
   <div class="container">
     <div class="row">
-      <h2>Signin</h2>
+      <h2>Sign in</h2>
       <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 col-md-offset-3" />
       <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-md-offset-3">
         <input type="email" v-model="formData.email" class="form-control" placeholder="email" />
         <br />
-        <input
-          type="password"
-          v-model="formData.password"
-          class="form-control"
-          placeholder="password"
-        />
+        <input type="password" v-model="formData.password" class="form-control" placeholder="password" />
         <br />
-        <button class="btn btn-success btn-block full-width" @click="signIn">Signin</button>
+        <button class="btn btn-success btn-block full-width" @click="signIn">Sign in</button>
         <br />
+        <button class="btn ui blue button" @click="signInWithGoogle"><span><i class="google icon"></i></span>Sign in
+          with Google</button>
         <br />
-        <button
-          class="btn btn-success btn-block full-width"
-          @click="signInWithGoogle"
-        >Signin with google</button>
+        <button class="btn ui facebook button " @click="signInWithFacebook"><span><i
+              class="facebook f icon"></i></span>Sign in with Facebook </button>
+        <br />
+        <button class="btn disabled ui twitter button " @click="signInWithTwitter">Sign in with Twitter</button>
       </div>
     </div>
     <br />
@@ -27,58 +24,76 @@
 </template>
 
 <script>
-import {
-  getAuth,
-  signInWithEmailAndPassword,
-  GoogleAuthProvider,
-  signInWithPopup,
-} from "firebase/auth";
-import userStore from "../store/user";
-import { login } from "../services/user.service";
+  import {
+    getAuth,
+    signInWithEmailAndPassword,
+    GoogleAuthProvider,
+    FacebookAuthProvider,
+    signInWithPopup,
+  } from "firebase/auth";
+  import userStore from "../store/user";
+  import {
+    login
+  } from "../services/user.service";
 
-export default {
-  name: "SignIn",
-  data() {
-    return {
-      formData: {
-        email: "bob@mail.com",
-        password: "123456",
+  export default {
+    name: "SignIn",
+    data() {
+      return {
+        formData: {
+          email: "bob@mail.com",
+          password: "123456",
+        },
+      };
+    },
+    methods: {
+      signIn() {
+        const auth = getAuth();
+        signInWithEmailAndPassword(
+            auth,
+            this.formData.email,
+            this.formData.password
+          )
+          .then((user) => {
+            login(user.user);
+            this.$router.replace("/");
+          })
+          .catch((err) => {
+            alert(err.message);
+          });
       },
-    };
-  },
-  methods: {
-    signIn() {
-      const auth = getAuth();
-      signInWithEmailAndPassword(
-        auth,
-        this.formData.email,
-        this.formData.password
-      )
-        .then((user) => {
-          login(user.user);
-          this.$router.replace("/");
-        })
-        .catch((err) => {
-          alert(err.message);
-        });
+      signInWithGoogle() {
+        const auth = getAuth();
+        const provider = new GoogleAuthProvider();
+        signInWithPopup(auth, provider)
+          .then((result) => {
+            const user = result.user;
+            console.log(user);
+            login(user);
+            this.$router.replace("/");
+          })
+          .catch((err) => {
+            alert(err.message);
+          });
+      },
+      signInWithFacebook() {
+        const auth = getAuth();
+        const provider = new FacebookAuthProvider();
+        signInWithPopup(auth, provider)
+          .then((result) => {
+            const user = result.user;
+            console.log(user);
+            login(user);
+            this.$router.replace("/");
+          })
+          .catch((err) => {
+            alert(err.message);
+          });
+      },
     },
-    signInWithGoogle() {
-      const auth = getAuth();
-      const provider = new GoogleAuthProvider();
-      signInWithPopup(auth, provider)
-        .then((result) => {
-          const user = result.user;
-          console.log(user);
-          login(user);
-          this.$router.replace("/");
-        })
-        .catch((err) => {
-          alert(err.message);
-        });
-    },
-  },
-};
+  };
 </script>
 
 <style>
+
 </style>
