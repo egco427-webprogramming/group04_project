e92b7c8d1e843e873144cda96cef85329398cf86
Nicharee  Chalermsuksri <nicharee.chl@student.mahidol.edu>
2022-04-25 08:57:26 +0700

    edit style

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,5 +15,15 @@
 </script>
     
 
-<style scoped>
+<style>
+#upper-segment {
+  display: grid;
+  grid-template-columns: 1fr 13fr 2fr;
+  gap: 1%;
+  align-items: center;
+  justify-items: center;
+}
+#upper-segment :nth-child(2) {
+  width: 100%;
+}
 </style>
