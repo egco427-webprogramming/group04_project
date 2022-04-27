7cdecc01e9f17f9311c14a4c72a557c9a2d96c5e
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-04-27 20:31:22 +0700

    split user history component

---
+++ src/components/UserHistoryTransaction.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="ui celled grid shadow">
+    <div class="row">
+      <!-- <UserHistoryTransactionItemList :items="transaction?.items" /> -->
+      <hr />
+
+      <div>
+        <p class="product-amount">Total Price:{{transaction?.price}}</p>
+        <p>Date: {{transaction?.date}}</p>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import UserHistoryTransactionItemList from "./UserHistoryTransactionItemList.vue";
+export default {
+  props: {
+    transaction: Object,
+  },
+  components: { UserHistoryTransactionItemList },
+};
+</script>
+<style>
+</style>
---
+++ src/components/UserHistoryTransactionItemList.vue
@@ -0,0 +1,24 @@
+<template >
+  <template v-for="item,idx in items">
+    <div class="three wide column">
+    </div>
+    <div class="ten wide column left aligned detail">
+      <div class="item">
+      </div>
+      <br />
+      <br />
+    </div>
+    <div class="three wide center aligned column">
+      <span>THB {{String(Math.round(item?.price))}}</span>
+      <!-- <span>THB {{String(Math.round(item?.price*item?.amount))}}</span> -->
+    </div>
+  </template>
+</template>
+<script>
+export default {
+  props: { items: Object },
+};
+</script>
+
+<style>
+</style>
