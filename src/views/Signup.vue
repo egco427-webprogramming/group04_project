de57bd655fe360f1705dd13bfd7f63aebba353c4
Jao <Jaopatk@gmail.com>
2022-04-28 23:25:56 +0700

    styling

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,18 +1,19 @@
 <template>
-    <div class="row">
-        <h2>SignUp</h2>
+    <div class="row signup">
+        <h2 class="text-signup">SignUp</h2>
         <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3 col-md-offset-3" />
         <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-md-offset-3">
-            <input type="email" v-model="formData.email" class="form-control" placeholder="email">
+                  <div align="center">
+            <input type="email" v-model="formData.email" class="form-control" placeholder="email" id="email-button" />
             <br>
-            <input type="password" v-model="formData.password" class="form-control" placeholder="password">
+            <input type="password" v-model="formData.password" class="form-control" placeholder="password" id="password-button" />
             <br>
-            <button class="btn btn-success" @click="signUp">SignUp</button>
+            <button class="btn btn-success" @click="signUp" id="signin-button">SignUp</button>
             <p>Already a member?
                 <router-link :to="{path:'/signin'}">
                     <span class="signup">Sign in</span>
                 </router-link>
-            </p>
+            </p></div>
         </div>
     </div>
 </template>
@@ -55,5 +56,35 @@
 </script>
 
 <style>
+  .signup {
+    margin-top: 100px;
+     margin-bottom: 100px;
+  }
+
+  .text-signup {
+    font-weight: 550;
+    font-size: 40px;
+  }
+
+  #email-button {
+    border-radius: 12px;
+    height: 50px;
+    width: 350px;
+    margin-top: 30px;
+  }
+
+  #password-button {
+    border-radius: 12px;
+    height: 50px;
+    width: 350px;
+    margin-top: 15px;
+  }
+
+  #signup-button {
+    height: 43px;
+    width: 350px;
+    border-radius: 30px;
+    margin-top: 35px;
+  }
 
 </style>
