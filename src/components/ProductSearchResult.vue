58192e6830f4f4f76446873d21d67d9d124ebd79
etnk125 <94974562+etnk125@users.noreply.github.com>
2022-04-29 16:26:38 +0700

    search page

---
+++ src/components/ProductSearchResult.vue
@@ -0,0 +1,57 @@
+<template>
+  <br />
+  <div>
+    <h1 class="text-query">search result of {{query}}</h1>
+  </div>
+  <div class="product-wrapper">
+    <template v-for="product in products">
+      <PromotionCard
+        v-if="product.promotion > 0"
+        :product="product"
+        :key="'product-card-'+product._id"
+      />
+      <ProductCard v-else :product="product" :key="'product-card-'+product._id" />
+    </template>
+  </div>
+  <br />
+</template>
+
+<script>
+import ProductCard from "./ProductCard.vue";
+import PromotionCard from "./PromotionCard.vue";
+import { useRoute } from "vue-router";
+import { computed, onMounted, ref, watchEffect, watch } from "vue";
+import { getProductListWithKeyword } from "../services/product.service";
+
+export default {
+  components: { ProductCard, PromotionCard },
+  async setup() {
+    const route = useRoute();
+
+    const fetchData = async (query) => await getProductListWithKeyword(query);
+
+    const products = ref(await fetchData(route.query.q));
+
+    watch(
+      () => route.query.q,
+      async (query, _) => {
+        products.value = await fetchData(query);
+      }
+    );
+    return { products, query: computed(() => route.query.q) };
+  },
+};
+</script>
+
+<style scoped>
+.product-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin-top: 40px;
+}
+.text-query {
+  font-weight: 700;
+  font-size: 50px;
+}
+</style>
